b1f1ddd22360b752f69f39d864ff8d3f75387ba5
landry <yvesengoulou@yahoo.fr>
2024-09-17 17:15:34 +0200

    feat: add logging to django for local dev

--- config/settings/base.py
+++ config/settings/base.py
@@ -55,7 +55,10 @@ THIRD_PARTY_APPS = [
     "django_celery_beat"
 ]
 
-LOCAL_APPS = []
+LOCAL_APPS = [
+    "core_apps.users",
+    "core_apps.profiles"
+]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
@@ -96,8 +99,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': getenv("POSTGRES_DB"),
+        "USER": getenv("POSTGRES_USER"),
+        "PASSWORD": getenv("POSTGRES_PASSWORD"),
+        "HOST": getenv("POSTGRES_HOST"),
+        "PORT": getenv("POSTGRES_PORT")
     }
 }
 
