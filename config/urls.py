9a5b3985e7a014642438fca05238307c54237955
landry <yvesengoulou@yahoo.fr>
2024-09-19 15:50:57 +0200

    feat: add custom user model, setup redoc api docs

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+       title="AMAGroup Apartements API",
+       default_version="V1",
+       description="An Apartment Management API for Real Estate",
+       contact=openapi.Contact(email="admin@amaestate.com"),
+       license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=[AllowAny],
+)
 
 urlpatterns = [
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path(settings.ADMIN_URL, admin.site.urls),
 ]
+
+admin.site.site_header ="AMAGroup Apartments Admin"
+admin.site.site_title = "AMAGroup Apartments Admin Portal"
+admin.site.index_title =" Welcome to AMAGroup Apartements Admin Portal"
