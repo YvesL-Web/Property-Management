e099480640d2b994c37d6ee37be8cb83f03d252a
landry <yvesengoulou@yahoo.fr>
2024-09-21 16:40:34 +0200

    feat: serve django api and next.js client with NGINX

--- config/settings/local.py
+++ config/settings/local.py
@@ -16,6 +16,8 @@ SITE_NAME = getenv("SITE_NAME")
 
 SECRET_KEY = getenv("DJANGO_SECRET_KEY","PFmcRbKqcaiDL9mahtyJbRcDOzNB0nqFPLvD-k6H1_WyrDLj5UM")
 
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
+
 ALLOWED_HOSTS = ["localhost","127.0.0.1","0.0.0.0"]
 
 ADMIN_URL = getenv("DJANGO_ADMIN_URL")
