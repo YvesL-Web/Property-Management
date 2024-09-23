659b20230a9d721a7a496039e32326ce1c282fae
landry <yvesengoulou@yahoo.fr>
2024-09-23 17:30:04 +0200

    feat: add TimeStamped, Profile models and  signals

--- config/settings/base.py
+++ config/settings/base.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from os import getenv, path
 from dotenv import load_dotenv
 
+import cloudinary
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 
@@ -188,3 +190,15 @@ CELERY_TASK_TIME_LIMIT = 5*60
 CELERY_TASK_SOFT_TIME_LIMIT = 60
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 CELERY_WORKER_SEND_TASK_EVENTS = True
+
+
+# Cloudinary
+CLOUDINARY_CLOUD_NAME = getenv("CLOUDINARY_CLOUD_NAME")
+CLOUDINARY_API_KEY = getenv("CLOUDINARY_API_KEY")
+CLOUDINARY_API_SECRET = getenv("CLOUDINARY_API_SECRET")
+
+cloudinary.config(
+    cloud_name= CLOUDINARY_CLOUD_NAME,
+    api_key = CLOUDINARY_API_KEY,
+    api_secret = CLOUDINARY_API_SECRET,
+)
--- core_apps/common/admin.py
+++ core_apps/common/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.contenttypes.admin import GenericTabularInline
+from .models import ContentView
+
+
+@admin.register(ContentView)
+class CntentViewAdmin(admin.ModelAdmin):
+    list_display = ["content_object", "user", "viewer_ip", "created_at"]
+
+
+class ContentViewInline(GenericTabularInline):
+    model = ContentView
+    extra = 0
+    readonly_fields = ["user","viewer_ip","created_at"]
