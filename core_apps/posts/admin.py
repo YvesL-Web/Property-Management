8d4c4756cf3f62d39b1e6e2c42d2253f55779146
landry <yvesengoulou@yahoo.fr>
2024-10-01 16:00:00 +0200

    feat:add post, reply, bookmar, upvote functionnalities

--- core_apps/posts/admin.py
+++ core_apps/posts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.contenttypes.models import ContentType
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
 
-# Register your models here.
+from core_apps.common.admin import ContentViewInline
+from core_apps.common.models import ContentView
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title","tag_list","get_total_views"]
+    inlines = [ContentViewInline]
+
+    def get_total_views(self, obj):
+        content_type = ContentType.objects.get_for_model(obj)
+        views = ContentView.objects.filter(
+            content_type=content_type, object_id=obj.pkid
+        ).count()
+        return views
+    
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        return super().get_queryset(request).prefetch_related("tags")
+    
+    def tag_list(self, obj):
+        return ", ".join(o.name for o in obj.tags.all())
