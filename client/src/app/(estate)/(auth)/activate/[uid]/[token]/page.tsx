8a20f69276cf7cb1eb1c3ad6029f393d3fbeda34
landry <yvesengoulou@yahoo.fr>
2024-10-08 12:43:41 +0200

    Add Auth form Header component

---
+++ client/src/app/(estate)/(auth)/activate/[uid]/[token]/page.tsx
@@ -0,0 +1,9 @@
+
+const ActivationPage = () => {
+  return (
+    <div>
+        <h1 className="dark:text-pumpkin text text-6xl">Activation Page</h1>
+    </div>
+  )
+}
+export default ActivationPage
