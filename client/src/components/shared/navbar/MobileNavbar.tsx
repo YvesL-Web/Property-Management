1eecf777a37bc20246623150fbf6728c5f42c0c1
landry <yvesengoulou@yahoo.fr>
2024-10-26 12:45:16 +0200

    feat: adjust mobile version

--- client/src/components/shared/navbar/MobileNavbar.tsx
+++ client/src/components/shared/navbar/MobileNavbar.tsx
@@ -1,49 +1,57 @@
-"use client"
-import { Button } from "@/components/ui/button"
-import { Sheet, SheetClose, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet"
-import { leftNavLinks } from "@/constants"
-import { HomeModernIcon } from "@heroicons/react/24/solid"
-import Image from "next/image"
-import Link from "next/link"
-import { usePathname } from "next/navigation"
-import mobileMenu from "../../../assets/icons/mobile-menu.svg"
-
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+	Sheet,
+	SheetClose,
+	SheetContent,
+	SheetFooter,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
+} from "@/components/ui/sheet";
+import { HomeModernIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import mobileMenu from "../../../assets/icons/mobile-menu.svg";
+import { useAuthNavigation } from "@/hooks";
 
 function LeftNavContent() {
-    const pathname = usePathname()
-    return (
-        <section className="flex h-full flex-col gap-6 pt-16">
-            {leftNavLinks.map((linkItem) => {
-                const isActive  = (pathname.includes(linkItem.path) && linkItem.path.length > 1) || pathname === linkItem.path
-                return (
-                    <SheetClose asChild key={linkItem.path}>
-                        <Link
-                            href={linkItem.path}
-                            className={`${isActive ? "electricIndigo-gradient text-babyPowder rounded-lg" : "text-baby_richBlack"} flex items-center justify-start gap-4 bg-transparent p-4`}
-                        >
-                            <Image
-                                src={linkItem.imgLocation}
-                                alt={linkItem.label}
-                                width={22}
-                                height={22}
-                                className={`${isActive ? "" : "color-invert"}`}
-                            />
-                            <p
-                                className={`${isActive ? "base-bold" : "base-medium"}`}
-                            >
-                                {linkItem.label}
-                            </p>
-                        </Link>
-                    </SheetClose>
-                );
-            })}
-        </section>
-    )
+	const pathname = usePathname();
+	const { filteredNavLinks } = useAuthNavigation();
+	return (
+		<section className="flex h-full flex-col gap-6 pt-16">
+			{filteredNavLinks.map((linkItem) => {
+				const isActive =
+					(pathname.includes(linkItem.path) && linkItem.path.length > 1) ||
+					pathname === linkItem.path;
+				return (
+					<SheetClose asChild key={linkItem.path}>
+						<Link
+							href={linkItem.path}
+							className={`${isActive ? "electricIndigo-gradient text-babyPowder rounded-lg" : "text-baby_richBlack"} flex items-center justify-start gap-4 bg-transparent p-4`}
+						>
+							<Image
+								src={linkItem.imgLocation}
+								alt={linkItem.label}
+								width={22}
+								height={22}
+								className={`${isActive ? "" : "color-invert"}`}
+							/>
+							<p className={`${isActive ? "base-bold" : "base-medium"}`}>
+								{linkItem.label}
+							</p>
+						</Link>
+					</SheetClose>
+				);
+			})}
+		</section>
+	);
 }
 
-
 const MobileNavbar = () => {
-  return (
+	const { handleLogout, isAuthenticated } = useAuthNavigation();
+	return (
 		<Sheet>
 			<SheetTrigger asChild className="cursor-pointer">
 				<Image
@@ -69,23 +77,32 @@ const MobileNavbar = () => {
 					<LeftNavContent />
 					<SheetClose asChild>
 						<SheetFooter>
-							<>
-								<Link href="/register">
-									<Button className="electricIndigo-gradient small-medium light-border-2 btn-tertiary text-babyPowder mt-4 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
-										Register
-									</Button>
-								</Link>
-								<Link href="/login">
-									<Button className="lime-gradient small-medium light-border-2 btn-tertiary text-babyPowder min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
-										Login
-									</Button>
-								</Link>
-							</>
+							{isAuthenticated ? (
+								<Button
+									onClick={handleLogout}
+									className="lime-gradient small-medium light-border-2 btn-tertiary text-baby_richBlack min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none"
+								>
+									Logout
+								</Button>
+							) : (
+								<div className="mt-4">
+									<Link href="/register">
+										<Button className="electricIndigo-gradient small-medium light-border-2 btn-tertiary text-babyPowder mt-4 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
+											Register
+										</Button>
+									</Link>
+									<Link href="/login">
+										<Button className="lime-gradient small-medium light-border-2 btn-tertiary text-babyPowder mt-3 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
+											Login
+										</Button>
+									</Link>
+								</div>
+							)}
 						</SheetFooter>
 					</SheetClose>
 				</div>
 			</SheetContent>
 		</Sheet>
 	);
-}
-export default MobileNavbar
+};
+export default MobileNavbar;
